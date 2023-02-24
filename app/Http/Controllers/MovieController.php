29ff534fc8a72b7bd32f6aa0c2e09fb3c15fc196
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-24 17:32:13 +0100

    Added cards dinamically taking everything from db

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -10,6 +11,9 @@ class MovieController extends Controller
 
     public function index()
     {
-        return view('layouts.main');
+
+        $movies = Movie::all();
+
+        return view('layouts.main', compact('movies'));
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    @vite('resources/js/app.js')
 </head>
-<body>
-    <h1>
-        Ciao a tutti
+<body class="py-5 bg-dark">
+    <h1 class="text-center text-success">
+        Movies
     </h1>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+            <div class="col-3 g-5 text-center">
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$movie->title}}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+                      <div class="card-text"> language: <strong>{{$movie->nationality}}</strong> </div>
+                      <div class="card-text"> Release: {{$movie->date}} </div>
+                      <div class="card-text"> Vote:  <strong>{{$movie->vote}}</strong> </div>
+                      <a href="#" class="card-link">See more</a>
+                    </div>
+                  </div>
+            </div>
+            @endforeach
+        </div>
+    </div>  
+
 </body>
 </html>
